643297420938488b56c12997be5443700651b2f3
Jef King <jef@agilebusinesscloud.com>
2014-08-19 22:17:39 -0700

    upload method

---
+++ King.Azure.Imaging/Models/RawData.cs
@@ -0,0 +1,17 @@
+﻿namespace King.Azure.Imaging.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class RawData
+    {
+        public byte[] Contents { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        public int FileSize { get; set; }
+        public Guid Identifier { get; set; }
+    }
+}
---
+++ King.Azure.Imaging/Web/Upload.cs
@@ -0,0 +1,36 @@
+﻿namespace King.Azure.Imaging.Web
+{
+    using King.Azure.Imaging.Models;
+    using System;
+    using System.Threading.Tasks;
+    using System.Web;
+
+    public class Upload
+    {
+        public async Task<RawData> Something(HttpRequest request)
+        {
+            var raw = new RawData()
+            {
+                Identifier = Guid.NewGuid(),
+            };
+
+            if (request.Files.Count > 0)
+            {
+                var file = request.Files[0];
+                raw.Contents = new byte[file.ContentLength];
+                await file.InputStream.ReadAsync(raw.Contents, 0, file.ContentLength);
+                raw.ContentType = file.ContentType;
+                raw.FileName = file.FileName;
+            }
+            else if (request.ContentLength > 0)
+            {
+                raw.Contents = new byte[request.ContentLength];
+                await request.InputStream.ReadAsync(raw.Contents, 0, request.ContentLength);
+                raw.FileName = request.Headers["X-File-Name"];
+                raw.ContentType = request.Headers["X-File-Type"];
+            }
+
+            return raw;
+        }
+    }
+}
