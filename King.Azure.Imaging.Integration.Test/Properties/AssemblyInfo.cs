a292f92411707a6597d35b2d2ae06cac605abc0a
Jef King <jef@agilebusinesscloud.com>
2014-08-19 21:40:10 -0700

    Test Projects

---
+++ King.Azure.Imaging.Integration.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Azure.Imaging.Integration.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Azure.Imaging.Integration.Test")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)][assembly: Guid("2f397762-381e-4502-b3e8-319b2ec3608e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ King.Azure.Imaging.Unit.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Azure.Imaging.Unit.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Azure.Imaging.Unit.Test")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4738253b-0629-47ca-aaae-e312805bddc6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
