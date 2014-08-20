3c6c161b06cdd4a5ea3628c51bce76f77431fa24
Jef King <jef@agilebusinesscloud.com>
2014-08-19 22:05:44 -0700

    initialization

---
+++ King.Azure.Imaging.Integration.Test/Initialization.cs
@@ -0,0 +1,36 @@
+﻿namespace King.Azure.Imaging.Integration.Test
+{
+    using NUnit.Framework;
+    using System.Configuration;
+    using System.Diagnostics;
+
+    [SetUpFixture]
+    public class Initialization
+    {
+        [SetUp]
+        public static void SetUp()
+        {
+            var emulator = ConfigurationManager.AppSettings["AzureEmulator"];
+
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessStartInfo(emulator, "start");
+                process.Start();
+
+                process.WaitForExit();
+            }
+        }
+
+        private static ProcessStartInfo CreateProcessStartInfo(string fileName, string arguments)
+        {
+            return new ProcessStartInfo(fileName, arguments)
+            {
+                UseShellExecute = false,
+                ErrorDialog = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+            };
+        }
+    }
+}
