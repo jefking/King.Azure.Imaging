27690adc4c305bf8dc1d7c8d297c709fd971f14b
Jef King <jef@agilebusinesscloud.com>
2014-08-24 22:02:27 -0700

    MVC demo

---
+++ King.Azure.Imaging.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="King.Azure.Imaging.Mvc.MvcApplication" Language="C#" %>
---
+++ King.Azure.Imaging.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(King.Azure.Imaging.Mvc.Startup))]
+namespace King.Azure.Imaging.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
