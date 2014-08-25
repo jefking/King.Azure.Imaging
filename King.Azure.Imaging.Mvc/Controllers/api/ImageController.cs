ae6240ffa08e1756b9e3ce6c8e4e82a964bdf466
Jef King <jef@agilebusinesscloud.com>
2014-08-24 22:28:48 -0700

    major overhaul

--- King.Azure.Imaging.Mvc/Controllers/api/ImageController.cs
+++ King.Azure.Imaging.Mvc/Controllers/api/ImageController.cs
@@ -1,6 +1,7 @@
 ﻿namespace King.Azure.Imaging.Mvc.Controllers.api
 {
-    using King.Azure.Imaging.Web;
+    using King.Azure.Data;
+    using King.Azure.Imaging.Models;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -11,11 +12,39 @@
 
     public class ImageController : ApiController
     {
-        private readonly Upload uploader = new Upload();
+        #region Members
+        /// <summary>
+        /// File Name Header
+        /// </summary>
+        public const string FileNameHeader = "X-File-Name";
+
+        /// <summary>
+        /// Content Type Header
+        /// </summary>
+        public const string ContentTypeHeader = "X-File-Type";
+
+        /// <summary>
+        /// Blob Container
+        /// </summary>
+        private readonly Container container = new Container("name", "connection string");
+        #endregion
 
         public async Task UploadImage()
         {
-            var image = uploader.Load(Request);
+            var data = new RawData()
+            {
+                Contents = await Request.Content.ReadAsByteArrayAsync(),
+                Identifier = Guid.NewGuid(),
+                ContentType = Request.Headers.GetValues(ContentTypeHeader).FirstOrDefault(),
+                FileName = Request.Headers.GetValues(FileNameHeader).FirstOrDefault(),
+            };
+            data.FileSize = data.Contents.Length;
+
+            //Store meta data to table?
+            //Store meta data to blob.
+            //Queue for additional processing.
+
+            await container.Save(data.Identifier.ToString(), data.Contents, data.ContentType);
         }
     }
 }
