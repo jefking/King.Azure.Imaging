40cb50741873ae3488e24017c24ae110a750d0e8
Jef King <jef@agilebusinesscloud.com>
2014-08-24 22:05:08 -0700

    Layout MVC

---
+++ King.Azure.Imaging.Mvc/Controllers/api/ImageController.cs
@@ -0,0 +1,13 @@
+﻿namespace King.Azure.Imaging.Mvc.Controllers.api
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class ImageController : ApiController
+    {
+    }
+}
