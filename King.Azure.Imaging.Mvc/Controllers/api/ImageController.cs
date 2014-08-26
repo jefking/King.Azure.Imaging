83147ac7f1494444107e994e0989453e82831391
Jef King <jef@agilebusinesscloud.com>
2014-08-25 20:47:27 -0700

    tweaks

--- King.Azure.Imaging.Mvc/Controllers/api/ImageController.cs
+++ King.Azure.Imaging.Mvc/Controllers/api/ImageController.cs
@@ -3,10 +3,7 @@
     using King.Azure.Data;
     using King.Azure.Imaging.Models;
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Net;
-    using System.Net.Http;
     using System.Threading.Tasks;
     using System.Web.Http;
 
