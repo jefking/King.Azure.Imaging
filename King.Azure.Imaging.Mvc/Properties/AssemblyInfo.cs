fa0c1a2b83cd851e71aed42ee50cdb08ba7ff7af
Jef King <jef@agilebusinesscloud.com>
2014-09-06 10:10:02 -0700

    versions

--- King.Azure.Imaging.Mvc/Properties/AssemblyInfo.cs
+++ King.Azure.Imaging.Mvc/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("397843b1-59e5-433e-b1fa-0ed9ba05388b")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
