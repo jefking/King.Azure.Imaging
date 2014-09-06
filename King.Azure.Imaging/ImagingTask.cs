3f7b8657d46bc1d713cbcd6bc5537bb0a05e7293
Jef King <jef@agilebusinesscloud.com>
2014-09-06 15:33:48 -0700

    Imaging Task

---
+++ King.Azure.Imaging/ImagingTask.cs
@@ -0,0 +1,20 @@
+﻿namespace King.Azure.Imaging
+{
+    using King.Azure.Data;
+    using King.Azure.Imaging.Models;
+    using King.Service.Data;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Imaging Task
+    /// </summary>
+    public class ImagingTask : Dequeue<ImageQueued>
+    {
+        #region Methods
+        protected override async Task Process(IQueued<ImageQueued> message)
+        {
+            await base.Process(message);
+        }
+        #endregion
+    }
+}
