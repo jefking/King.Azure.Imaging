c0369d097214b72403958e16c15e487b1f3fb887
Jef King <jef@agilebusinesscloud.com>
2014-09-07 09:59:38 -0700

    Display Images

--- King.Azure.Imaging.Mvc/Controllers/HomeController.cs
+++ King.Azure.Imaging.Mvc/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿namespace King.Azure.Imaging.Mvc.Controllers
 {
+    using King.Azure.Data;
+    using King.Azure.Imaging.Entities;
+    using System.Linq;
     using System.Web.Mvc;
 
     /// <summary>
@@ -7,10 +10,21 @@
     /// </summary>
     public class HomeController : Controller
     {
+        private static readonly IStorageElements elements = new StorageElements();
+        private readonly ITableStorage table = new TableStorage(elements.Table, "UseDevelopmentStorage=true");
+
         #region Methods
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Images()
+        {
+            var data = from t in table.QueryByRow<ImageEntity>("thumb")
+                   select t.FileName;
+
+            return View(data);
         }
         #endregion
     }
