0da8b29ad37978f3b8f0eb7de06da94d297752da
Jef King <jef@agilebusinesscloud.com>
2014-09-06 20:23:23 -0700

    Testing Classes

---
+++ King.Azure.Imaging.Unit.Test/StorageElementsTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace King.Azure.Imaging.Unit.Test
+{
+    class StorageElementsTests
+    {
+    }
+}
