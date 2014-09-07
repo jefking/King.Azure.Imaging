3c1a4b89ff9cc61e45755ba9a9596b9345bdd6eb
Jef King <jef@agilebusinesscloud.com>
2014-09-06 20:35:51 -0700

    test cases

--- King.Azure.Imaging.Unit.Test/ImageTaskFactoryTests.cs
+++ King.Azure.Imaging.Unit.Test/ImageTaskFactoryTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
+    using King.Service;
+    using NSubstitute;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
@@ -10,5 +12,39 @@
     [TestFixture]
     public class ImageTaskFactoryTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new ImageTaskFactory(Guid.NewGuid().ToString());
+        }
+
+        [Test]
+        public void IsITaskFactory()
+        {
+            Assert.IsNotNull(new ImageTaskFactory(Guid.NewGuid().ToString()) as ITaskFactory<object>);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorConnectionStringNull()
+        {
+            new ImageTaskFactory(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorStorageElementsNull()
+        {
+            var versions = Substitute.For<IVersions>();
+            new ImageTaskFactory(Guid.NewGuid().ToString(), null, versions);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorVersionsNull()
+        {
+            var elements = Substitute.For<IStorageElements>();
+            new ImageTaskFactory(Guid.NewGuid().ToString(), elements, null);
+        }
     }
 }
