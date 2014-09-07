c110fe74d03e0004433103ff6f47d27917caa542
Jef King <jef@agilebusinesscloud.com>
2014-09-06 20:47:41 -0700

    Tests

--- King.Azure.Imaging.Unit.Test/ImagePreprocessorTests.cs
+++ King.Azure.Imaging.Unit.Test/ImagePreprocessorTests.cs
@@ -10,5 +10,39 @@
     [TestFixture]
     public class ImagePreprocessorTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var connectionString = "UseDevelopmentStorage=true";
+            new ImagePreprocessor(connectionString);
+        }
+
+        [Test]
+        public void IsIImagePreprocessor()
+        {
+            var connectionString = "UseDevelopmentStorage=true";
+            Assert.IsNotNull(new ImagePreprocessor(connectionString) as IImagePreprocessor);
+        }
+
+        [Test]
+        public void Original()
+        {
+            Assert.AreEqual("original", ImagePreprocessor.Original);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorConnectionStringNull()
+        {
+            new ImagePreprocessor(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorElementsNull()
+        {
+            var connectionString = "UseDevelopmentStorage=true";
+            new ImagePreprocessor(connectionString, null);
+        }
     }
 }
--- King.Azure.Imaging.Unit.Test/ImageTaskFactoryTests.cs
+++ King.Azure.Imaging.Unit.Test/ImageTaskFactoryTests.cs
@@ -5,10 +5,7 @@
     using NSubstitute;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class ImageTaskFactoryTests
