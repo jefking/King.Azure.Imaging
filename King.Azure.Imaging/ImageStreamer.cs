f8c569e4c92e48bbc92c58ea52deeebaba9aae26
Jef King <jef@agilebusinesscloud.com>
2014-09-07 13:36:07 -0700

    Code Clean-Up

--- King.Azure.Imaging/ImageStreamer.cs
+++ King.Azure.Imaging/ImageStreamer.cs
@@ -1,20 +1,46 @@
 ﻿namespace King.Azure.Imaging
 {
     using King.Azure.Data;
+    using System;
     using System.IO;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Image Streamer
+    /// </summary>
     public class ImageStreamer : IImageStreamer
     {
-        private readonly IContainer container;
+        #region Members
+        /// <summary>
+        /// Container
+        /// </summary>
+        private readonly IContainer container = null;
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="container">Container</param>
         public ImageStreamer(IContainer container)
         {
             this.container = container;
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// Get File
+        /// </summary>
+        /// <param name="file">File</param>
+        /// <returns>Stream</returns>
         public async Task<Stream> Get(string file)
         {
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                throw new ArgumentException("file");
+            }
+
             var bytes = await container.Get(file);
             var ms = new MemoryStream();
             await ms.WriteAsync(bytes, 0, bytes.Length);
@@ -25,11 +51,17 @@
 
             return ms;
         }
+        #endregion
 
+        #region Properties
+        /// <summary>
+        /// Content Type
+        /// </summary>
         public string ContentType
         {
             private set;
             get;
         }
+        #endregion
     }
 }
