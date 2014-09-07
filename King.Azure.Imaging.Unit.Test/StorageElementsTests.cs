10f501701476daf5eae34179b932f3904daa6f9b
Jef King <jef@agilebusinesscloud.com>
2014-09-06 20:32:22 -0700

    Storage Elements Tested

--- King.Azure.Imaging.Unit.Test/StorageElementsTests.cs
+++ King.Azure.Imaging.Unit.Test/StorageElementsTests.cs
@@ -1,14 +1,41 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
     using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class StorageElementsTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new StorageElements();
+        }
+
+        [Test]
+        public void IStorageElements()
+        {
+            Assert.IsNotNull(new StorageElements() as IStorageElements);
+        }
+
+        [Test]
+        public void Container()
+        {
+            var data = new StorageElements();
+            Assert.AreEqual("images", data.Container);
+        }
+
+        [Test]
+        public void Queue()
+        {
+            var data = new StorageElements();
+            Assert.AreEqual("imaging", data.Queue);
+        }
+
+        [Test]
+        public void Table()
+        {
+            var data = new StorageElements();
+            Assert.AreEqual("imaging", data.Table);
+        }
     }
 }
