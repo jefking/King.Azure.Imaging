91c1cdf6066e6a0fc5c0f7a805f6adb8377e05b1
Jef King <jef@agilebusinesscloud.com>
2014-09-07 15:12:24 -0700

    more compact

--- King.Azure.Imaging.Mvc/Controllers/api/ImageController.cs
+++ King.Azure.Imaging.Mvc/Controllers/api/ImageController.cs
@@ -2,8 +2,6 @@
 {
     using King.Azure.Data;
     using System;
-    using System.IO;
-    using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading.Tasks;
@@ -13,59 +11,11 @@
     /// <summary>
     /// Image Controller
     /// </summary>
-    public class ImageController : ApiController
+    public class ImageController : ImageApiController
     {
-        #region Members
-        /// <summary>
-        /// Connection String
-        /// </summary>
-        private const string connectionString = "UseDevelopmentStorage=true";
-
-        /// <summary>
-        /// Image Preprocessor
-        /// </summary>
-        private readonly IImagePreprocessor preprocessor = new ImagePreprocessor(connectionString);
-
-        /// <summary>
-        /// Storage Elements
-        /// </summary>
-        private static readonly IStorageElements elements = new StorageElements();
-        #endregion
-
-        #region Methods
-        [HttpPost]
-        public async Task Post()
+        public ImageController()
+            : base("UseDevelopmentStorage=true")
         {
-            var request = HttpContext.Current.Request;
-            if (null != request.Files)
-            {
-                foreach (string index in request.Files)
-                {
-                    var file = request.Files[index];
-                    var contentType = file.ContentType;
-                    var fileName = file.FileName;
-                    var bytes = new byte[file.ContentLength];
-                    await file.InputStream.ReadAsync(bytes, 0, bytes.Length);
-                    await this.preprocessor.Process(bytes, contentType, fileName);
-                }
-            }
         }
-
-        [HttpGet]
-        public async Task<HttpResponseMessage> Get(string file)
-        {
-            if (string.IsNullOrWhiteSpace(file))
-            {
-                throw new ArgumentException("file");
-            }
-
-            var streamer = new ImageStreamer(new Container(elements.Container, connectionString));
-            var ms = await streamer.Get(file);
-            var response = new HttpResponseMessage();
-            response.Content = new StreamContent(ms);
-            response.Content.Headers.ContentType = new MediaTypeHeaderValue(streamer.ContentType);
-            return response;
-        }
-        #endregion
     }
 }
---
+++ King.Azure.Imaging/ImageApiController.cs
@@ -0,0 +1,83 @@
+﻿namespace King.Azure.Imaging
+{
+    using King.Azure.Data;
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Http;
+
+    /// <summary>
+    /// Image Api Controller
+    /// </summary>
+    public class ImageApiController : ApiController
+    {
+        #region Members
+        /// <summary>
+        /// Image Preprocessor
+        /// </summary>
+        private readonly IImagePreprocessor preprocessor = null;
+
+        /// <summary>
+        /// Storage Elements
+        /// </summary>
+        private readonly IStorageElements elements = null;
+
+        /// <summary>
+        /// Container
+        /// </summary>
+        private readonly IContainer container = null;
+        #endregion
+
+        #region Constructors
+        public ImageApiController(string connectionString)
+            : this(connectionString, new ImagePreprocessor(connectionString), new StorageElements())
+        {
+
+        }
+        public ImageApiController(string connectionString, IImagePreprocessor preprocessor, IStorageElements elements)
+        {
+            this.container = new Container(elements.Container, connectionString);
+            this.preprocessor = preprocessor;
+            this.elements = elements;
+        }
+        #endregion
+
+        #region Methods
+        [HttpPost]
+        public async Task Post()
+        {
+            var request = HttpContext.Current.Request;
+            if (null != request.Files)
+            {
+                foreach (string index in request.Files)
+                {
+                    var file = request.Files[index];
+                    var contentType = file.ContentType;
+                    var fileName = file.FileName;
+                    var bytes = new byte[file.ContentLength];
+                    await file.InputStream.ReadAsync(bytes, 0, bytes.Length);
+                    await this.preprocessor.Process(bytes, contentType, fileName);
+                }
+            }
+        }
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get(string file)
+        {
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                throw new ArgumentException("file");
+            }
+
+            var streamer = new ImageStreamer(this.container);
+            var ms = await streamer.Get(file);
+            var response = new HttpResponseMessage();
+            response.Content = new StreamContent(ms);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue(streamer.ContentType);
+            return response;
+        }
+        #endregion
+    }
+}
