b5c8cc955865a35ba7ac68e904126d368054675c
Jef King <jef@agilebusinesscloud.com>
2014-09-06 20:30:36 -0700

    Versions Tested

--- King.Azure.Imaging.Unit.Test/VersionsTests.cs
+++ King.Azure.Imaging.Unit.Test/VersionsTests.cs
@@ -1,14 +1,32 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
     using NUnit.Framework;
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class VersionsTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new Versions();
+        }
+
+        [Test]
+        public void IsIVersions()
+        {
+            Assert.IsNotNull(new Versions() as IVersions);
+        }
+
+        [Test]
+        public void Images()
+        {
+            var v = new Versions();
+            Assert.IsNotNull(v.Images);
+            Assert.AreEqual(3, v.Images.Keys.Count());
+            Assert.IsNotNull(v.Images["Thumb"]);
+            Assert.IsNotNull(v.Images["Medium"]);
+            Assert.IsNotNull(v.Images["Large"]);
+        }
     }
 }
