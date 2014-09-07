be35aa48c1c2ec9a45a84a5b9fb098d64c58200f
Jef King <jef@agilebusinesscloud.com>
2014-09-06 21:34:57 -0700

    Unit Test Coverage

--- King.Azure.Imaging.Unit.Test/ImagingProcessorTests.cs
+++ King.Azure.Imaging.Unit.Test/ImagingProcessorTests.cs
@@ -1,11 +1,13 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
     using King.Azure.Data;
+    using King.Azure.Imaging.Entities;
     using King.Azure.Imaging.Models;
     using NSubstitute;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Threading.Tasks;
 
     [TestFixture]
@@ -38,21 +40,30 @@
         [Test]
         public async Task Process()
         {
+            var bytes = File.ReadAllBytes(Environment.CurrentDirectory + @"\icon.png");
             var data = new ImageQueued()
             {
                 Identifier = Guid.NewGuid(),
                 FileNameFormat = "good_{0}_file",
             };
+            var versions = new Dictionary<string, string>();
+            versions.Add("temp", "width=100&height=100&crop=auto&format=jpg");
             var container = Substitute.For<IContainer>();
-            container.Get(string.Format(data.FileNameFormat, ImagePreprocessor.Original));
-            var table = Substitute.For<ITableStorage>();
+            container.Get(string.Format(data.FileNameFormat, ImagePreprocessor.Original)).Returns(Task.FromResult(bytes));
+            container.Save(string.Format(data.FileNameFormat, "temp"), Arg.Any<byte[]>(), "image/jpeg");
 
-            var ip = new ImagingProcessor(container, table, new Dictionary<string, string>());
+            var table = Substitute.For<ITableStorage>();
+            table.InsertOrReplace(Arg.Any<ImageEntity>());
+
+            var ip = new ImagingProcessor(container, table, versions);
             var result = await ip.Process(data);
 
             Assert.IsTrue(result);
 
             container.Received().Get(string.Format(data.FileNameFormat, ImagePreprocessor.Original));
+            container.Received().Save(string.Format(data.FileNameFormat, "temp"), Arg.Any<byte[]>(), "image/jpeg");
+
+            table.Received().InsertOrReplace(Arg.Any<ImageEntity>());
         }
     }
 }
