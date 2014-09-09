54df5338dca1e3f77dab7818fc09c274bcede2fc
Jef King <jef@agilebusinesscloud.com>
2014-09-08 19:54:44 -0700

    constructor tests

--- King.Azure.Imaging.Unit.Test/ImageApiControllerTests.cs
+++ King.Azure.Imaging.Unit.Test/ImageApiControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
+    using NSubstitute;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
@@ -23,6 +24,32 @@
         public void IsApiController()
         {
             Assert.IsNotNull(new ImageApiController(connectionString) as ApiController);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorConnectionStringNull()
+        {
+            var preprocessor = Substitute.For<IImagePreprocessor>();
+            var elements = Substitute.For<IStorageElements>();
+            new ImageApiController(null, preprocessor, elements);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorImagePreprocessorNull()
+        {
+            var preprocessor = Substitute.For<IImagePreprocessor>();
+            var elements = Substitute.For<IStorageElements>();
+            new ImageApiController(connectionString, null, elements);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorStorageElementsNull()
+        {
+            var preprocessor = Substitute.For<IImagePreprocessor>();
+            new ImageApiController(connectionString, preprocessor, null);
         }
     }
 }
