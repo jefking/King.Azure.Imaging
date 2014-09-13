f92a44f6ea5bc77d8aa587ff47b428acd94f3e39
Jef King <jef@agilebusinesscloud.com>
2014-09-13 08:57:16 -0700

    Tear Down

--- King.Azure.Imaging.Integration.Test/Initialization.cs
+++ King.Azure.Imaging.Integration.Test/Initialization.cs
@@ -21,6 +21,20 @@
             }
         }
 
+        [TearDown]
+        public static void TearDown()
+        {
+            var emulator = ConfigurationManager.AppSettings["AzureEmulator"];
+
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessStartInfo(emulator, "clear");
+                process.Start();
+
+                process.WaitForExit();
+            }
+        }
+
         private static ProcessStartInfo CreateProcessStartInfo(string fileName, string arguments)
         {
             return new ProcessStartInfo(fileName, arguments)
