0cb0c5614b91af33e0077d0248530ef13b361277
Jef King <jef@agilebusinesscloud.com>
2014-09-13 09:09:41 -0700

    Integration Test Classes

---
+++ King.Azure.Imaging.Integration.Test/ImagingProcessorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace King.Azure.Imaging.Integration.Test
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class ImagingProcessorTests
+    {
+    }
+}
