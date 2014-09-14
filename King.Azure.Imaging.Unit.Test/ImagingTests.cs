0c45298dde615a0eaa93f630c91ae97d692478b0
Jef King <jef@agilebusinesscloud.com>
2014-09-14 12:47:12 -0700

    testing resizing, unit tested

--- King.Azure.Imaging.Unit.Test/ImagingTests.cs
+++ King.Azure.Imaging.Unit.Test/ImagingTests.cs
@@ -1,8 +1,11 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
+    using ImageProcessor.Imaging.Formats;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
+    using System.Drawing;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -39,6 +42,21 @@
         }
 
         [Test]
+        public void Size()
+        {
+            var file = Environment.CurrentDirectory + @"\icon.png";
+            var bytes = File.ReadAllBytes(file);
+
+            var i = new Imaging();
+            var size = i.Size(bytes);
+
+            Assert.IsNotNull(size);
+            var bitMap = new Bitmap(file);
+            Assert.AreEqual(bitMap.Width, size.Width);
+            Assert.AreEqual(bitMap.Height, size.Height);
+        }
+
+        [Test]
         [ExpectedException(typeof(ArgumentException))]
         public void ResizeDataNull()
         {
@@ -60,6 +78,27 @@
         {
             var i = new Imaging();
             i.Resize(new byte[123], null);
+        }
+
+        [Test]
+        public void Resize()
+        {
+            var file = Environment.CurrentDirectory + @"\icon.png";
+            var bytes = File.ReadAllBytes(file);
+            var version = new ImageVersion()
+            {
+                Format = new GifFormat(),
+                Width = 10,
+                Height = 10,
+            };
+
+            var i = new Imaging();
+            var data = i.Resize(bytes, version);
+
+            Assert.IsNotNull(data);
+            var size = i.Size(data);
+            Assert.AreEqual(version.Width, size.Width);
+            Assert.AreEqual(version.Height, size.Height);
         }
     }
 }
