4df9859d0ad3dd93a45914e77bee22c296ccfb92
Jef King <jef@agilebusinesscloud.com>
2014-09-14 17:06:23 -0700

    Web API Get, Integration Tested

--- King.Azure.Imaging.Integration.Test/ImageApiControllerTests.cs
+++ King.Azure.Imaging.Integration.Test/ImageApiControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace King.Azure.Imaging.Integration.Test
 {
+    using King.Azure.Data;
+    using NSubstitute;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
@@ -10,5 +12,43 @@
     [TestFixture]
     public class ImageApiControllerTests
     {
+        private const string connectionString = "UseDevelopmentStorage=true";
+        private IContainer container;
+
+        [SetUp]
+        public void Setup()
+        {
+            var name = 'a' + Guid.NewGuid().ToString().Replace('-', 'a').ToLowerInvariant();
+            this.container = new Container(name, connectionString);
+            this.container.CreateIfNotExists().Wait();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.container.Delete().Wait();
+        }
+
+        [Test]
+        public async Task Get()
+        {
+            var random = new Random();
+            var bytes = new byte[128];
+            random.NextBytes(bytes);
+
+            var file = Guid.NewGuid().ToString();
+
+            await this.container.Save(file, bytes, "image/jpeg");
+
+            var preProcessor = Substitute.For<IImagePreprocessor>();
+            var streamer = new ImageStreamer(this.container);
+
+            var api = new ImageApiController(preProcessor, streamer);
+            var data = await api.Get(file);
+
+            Assert.IsNotNull(data);
+            Assert.AreEqual(bytes, await data.Content.ReadAsByteArrayAsync());
+            Assert.AreEqual("image/jpeg", data.Content.Headers.ContentType.MediaType);
+        }
     }
 }
