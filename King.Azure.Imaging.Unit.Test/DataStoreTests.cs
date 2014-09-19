0a28cf7616c87e540af06698d2f88922e59a7cb6
Jef King <jef@agilebusinesscloud.com>
2014-09-18 20:54:51 -0700

    100% code coverage again!

--- King.Azure.Imaging.Unit.Test/DataStoreTests.cs
+++ King.Azure.Imaging.Unit.Test/DataStoreTests.cs
@@ -1,15 +1,13 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
+    using ImageProcessor.Imaging.Formats;
     using King.Azure.Data;
     using King.Azure.Imaging.Entities;
     using King.Azure.Imaging.Models;
     using NSubstitute;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
     using System.Drawing;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
 
     [TestFixture]
@@ -170,7 +168,7 @@
             var quality = 100;
             var width = 122;
             var height = 133;
-            
+
             var imaging = Substitute.For<IImaging>();
             var container = Substitute.For<IContainer>();
             container.Save(fileName, content, mimeType);
@@ -186,6 +184,76 @@
             queue.Received().Save(Arg.Any<ImageQueued>());
             container.Received().Save(fileName, content, mimeType);
             table.Received().InsertOrReplace(Arg.Any<ImageEntity>());
+        }
+
+        [Test]
+        public async Task Resize()
+        {
+            var fileName = Guid.NewGuid().ToString();
+            var width = 122;
+            var format = new JpegFormat();
+            var id = Guid.NewGuid();
+            var versionName = Guid.NewGuid().ToString();
+            var cachedFileName = Guid.NewGuid().ToString();
+
+            var imaging = Substitute.For<IImaging>();
+            imaging.Get(Naming.DefaultExtension, 85).Returns(format);
+            imaging.Resize(Arg.Any<byte[]>(), Arg.Any<ImageVersion>());
+            var container = Substitute.For<IContainer>();
+            var table = Substitute.For<ITableStorage>();
+            var queue = Substitute.For<IStorageQueue>();
+            var naming = Substitute.For<INaming>();
+            naming.FromFileName(fileName).Returns(id);
+            naming.DynamicVersion(format.DefaultExtension, 85, width, 0).Returns(versionName);
+            naming.FileName(id, versionName, format.DefaultExtension).Returns(cachedFileName);
+
+            var store = new DataStore(imaging, container, table, queue, naming);
+            var result = await store.Resize(fileName, width);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Raw);
+            Assert.AreEqual(format.MimeType, result.MimeType);
+
+            naming.Received().FromFileName(fileName);
+            naming.Received().DynamicVersion(format.DefaultExtension, 85, width, 0);
+            naming.Received().FileName(id, versionName, format.DefaultExtension);
+            imaging.Received().Get(Naming.DefaultExtension, 85);
+            imaging.Received().Resize(Arg.Any<byte[]>(), Arg.Any<ImageVersion>());
+        }
+
+        [Test]
+        public async Task ResizeCache()
+        {
+            var fileName = Guid.NewGuid().ToString();
+            var width = 122;
+            var format = new JpegFormat();
+            var id = Guid.NewGuid();
+            var versionName = Guid.NewGuid().ToString();
+            var cachedFileName = Guid.NewGuid().ToString();
+
+            var imaging = Substitute.For<IImaging>();
+            imaging.Get(Naming.DefaultExtension, 85).Returns(format);
+            imaging.Resize(Arg.Any<byte[]>(), Arg.Any<ImageVersion>());
+            var container = Substitute.For<IContainer>();
+            var table = Substitute.For<ITableStorage>();
+            var queue = Substitute.For<IStorageQueue>();
+            var naming = Substitute.For<INaming>();
+            naming.FromFileName(fileName).Returns(id);
+            naming.DynamicVersion(format.DefaultExtension, 85, width, 0).Returns(versionName);
+            naming.FileName(id, versionName, format.DefaultExtension).Returns(cachedFileName);
+
+            var store = new DataStore(imaging, container, table, queue, naming);
+            var result = await store.Resize(fileName, width, 0, Naming.DefaultExtension, 85, true);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Raw);
+            Assert.AreEqual(format.MimeType, result.MimeType);
+
+            naming.Received().FromFileName(fileName);
+            naming.Received().DynamicVersion(format.DefaultExtension, 85, width, 0);
+            naming.Received().FileName(id, versionName, format.DefaultExtension);
+            imaging.Received().Get(Naming.DefaultExtension, 85);
+            imaging.Received().Resize(Arg.Any<byte[]>(), Arg.Any<ImageVersion>());
         }
     }
 }
