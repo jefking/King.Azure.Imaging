99451b3e06f0d678da65616a252e276c916ca1da
Jef King <jef@agilebusinesscloud.com>
2014-12-20 21:26:02 -0800

    Test cases

---
+++ King.Azure.Imaging.Unit.Test/Tasks/ImageDequeueSetupTests.cs
@@ -0,0 +1,48 @@
+﻿namespace King.Azure.Imaging.Unit.Test.Tasks
+{
+    using King.Azure.Imaging.Models;
+    using King.Azure.Imaging.Tasks;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class ImageDequeueSetupTests
+    {
+        private const string connectionString = "UseDevelopmentStorage=true";
+
+        [Test]
+        public void Constructor()
+        {
+            var config = new TaskConfiguration()
+            {
+                Versions = new Versions(),
+                StorageElements = new StorageElements(),
+            };
+
+            new ImageDequeueSetup(config);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorConfigNull()
+        {
+            new ImageDequeueSetup(null);
+        }
+
+        [Test]
+        public void Get()
+        {
+            var config = new TaskConfiguration()
+            {
+                Versions = new Versions(),
+                StorageElements = new StorageElements(),
+                ConnectionString = connectionString,
+            };
+
+            var setup = new ImageDequeueSetup(config);
+            var p = setup.Get();
+
+            Assert.IsNotNull(p as Processor);
+        }
+    }
+}
---
+++ King.Azure.Imaging/Tasks/ImageDequeueSetup.cs
@@ -0,0 +1,55 @@
+﻿namespace King.Azure.Imaging.Tasks
+{
+    using King.Azure.Data;
+    using King.Azure.Imaging.Models;
+    using King.Service.Data;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Image Dequeue Setup
+    /// </summary>
+    public class ImageDequeueSetup : QueueSetup<ImageQueued>
+    {
+        #region Constructors
+        /// <summary>
+        /// Image Dequeue Setup
+        /// </summary>
+        /// <param name="config"></param>
+        public ImageDequeueSetup(ITaskConfiguration config)
+        {
+            if (null == config)
+            {
+                throw new ArgumentNullException("config");
+            }
+            
+            this.Images = config.Versions.Images;
+            this.ConnectionString = config.ConnectionString;
+            this.Name = config.StorageElements.Queue;
+            this.Priority = config.Priority;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Image Versions to Generate
+        /// </summary>
+        public IReadOnlyDictionary<string, IImageVersion> Images
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Get Processor
+        /// </summary>
+        /// <returns>Image Processor</returns>
+        public override IProcessor<ImageQueued> Get()
+        {
+            return new Processor(new DataStore(this.ConnectionString), this.Images);
+        }
+        #endregion
+    }
+}
