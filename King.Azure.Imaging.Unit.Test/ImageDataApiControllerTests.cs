26d72a12410eb3f6d6ec755117b621948119df3c
Jef King <jef@agilebusinesscloud.com>
2014-12-29 16:11:03 -0800

    base test

--- King.Azure.Imaging.Unit.Test/ImageDataApiControllerTests.cs
+++ King.Azure.Imaging.Unit.Test/ImageDataApiControllerTests.cs
@@ -5,9 +5,7 @@
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Net;
-    using System.Text;
     using System.Threading.Tasks;
     using System.Web.Http;
 
--- King.Azure.Imaging.Unit.Test/QueryDataStoreTests.cs
+++ King.Azure.Imaging.Unit.Test/QueryDataStoreTests.cs
@@ -1,14 +1,89 @@
 ﻿namespace King.Azure.Imaging.Unit.Test
 {
+    using King.Azure.Data;
+    using King.Azure.Imaging.Models;
+    using Microsoft.WindowsAzure.Storage.Table;
+    using NSubstitute;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     [TestFixture]
     public class QueryDataStoreTests
     {
+        private const string connectionString = "UseDevelopmentStorage=true";
+
+        [Test]
+        public void Constructor()
+        {
+            new QueryDataStore(connectionString);
+        }
+
+        [Test]
+        public void ConstructorElements()
+        {
+            new QueryDataStore(connectionString, new StorageElements());
+        }
+
+        [Test]
+        public void ConstructorQueryDataStore()
+        {
+            var table = Substitute.For<ITableStorage>();
+            new QueryDataStore(table);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorQueryDataStoreNull()
+        {
+            new QueryDataStore((ITableStorage)null);
+        }
+
+        [Test]
+        public void IsIQueryDataStore()
+        {
+            Assert.IsNotNull(new QueryDataStore(connectionString) as IQueryDataStore);
+        }
+
+        [Test]
+        public async Task Query()
+        {
+            var id = Guid.NewGuid();
+            var version = Guid.NewGuid().ToString();
+            var fileName = Guid.NewGuid().ToString();
+            var etag = Guid.NewGuid().ToString();
+            var createdOn = DateTime.UtcNow;
+
+            var items = new List<IDictionary<string, object>>();
+            var dic = new Dictionary<string, object>();
+            dic.Add(TableStorage.PartitionKey, id);
+            dic.Add(TableStorage.RowKey, version);
+            dic.Add(TableStorage.Timestamp, createdOn);
+            dic.Add(TableStorage.ETag, etag);
+            dic.Add("FileName", fileName);
+
+            items.Add(dic);
+
+            var table = Substitute.For<ITableStorage>();
+            table.Query(Arg.Any<TableQuery>()).Returns(Task.FromResult<IEnumerable<IDictionary<string, object>>>(items));
+
+            var store = new QueryDataStore(table);
+            var r = await store.Query(id, version, fileName);
+
+            Assert.IsNotNull(r);
+            Assert.AreEqual(1, r.Count());
+            var d = r.First();
+            Assert.IsFalse(d.ContainsKey(TableStorage.PartitionKey));
+            Assert.IsFalse(d.ContainsKey(TableStorage.RowKey));
+            Assert.IsFalse(d.ContainsKey(TableStorage.Timestamp));
+            Assert.IsFalse(d.ContainsKey(TableStorage.ETag));
+            Assert.AreEqual(id, d["Identifier"]);
+            Assert.AreEqual(version, d["Version"]);
+            Assert.AreEqual(createdOn, d["CreatedOn"]);
+
+            table.Received().Query(Arg.Any<TableQuery>());
+        }
     }
 }
