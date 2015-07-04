7df9af4acb19e145eb35043f6d1536895b96a52c
Jef King <jef@agilebusinesscloud.com>
2015-07-03 17:19:08 -0700

    Web job project

---
+++ King.Azure.Imaging.WebJob/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ComputeWebJobsSDKQueue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ComputeWebJobsSDKQueue")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a6e4ebed-90c1-4c3b-8a0a-674b827c0eff")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
