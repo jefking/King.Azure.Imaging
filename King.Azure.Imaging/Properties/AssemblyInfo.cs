831bc33861b6054ab776299a8d83012ae485372b
Jef King <jef@agilebusinesscloud.com>
2015-07-05 08:29:13 -0700

    cr

--- King.Azure.Imaging/Properties/AssemblyInfo.cs
+++ King.Azure.Imaging/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("King.Azure.Imaging")]
-[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
