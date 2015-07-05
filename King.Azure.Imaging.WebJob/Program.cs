cc869b9a55e5d76be2fd629e7bfe7d5a6ab54e21
Jef King <jef@agilebusinesscloud.com>
2015-07-05 10:16:37 -0700

    less

--- King.Azure.Imaging.WebJob/Program.cs
+++ King.Azure.Imaging.WebJob/Program.cs
@@ -8,29 +8,21 @@
     {
         public static void Main()
         {
-            if (!VerifyConfiguration())
-            {
-                Console.ReadLine();
-                return;
-            }
-            
-            var host = new JobHost();
-            host.RunAndBlock();
-        }
-
-        private static bool VerifyConfiguration()
-        {
             var webJobsDashboard = ConfigurationManager.ConnectionStrings["AzureWebJobsDashboard"].ConnectionString;
             var webJobsStorage = ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString;
-
-            var configOK = true;
+            
             if (string.IsNullOrWhiteSpace(webJobsDashboard) || string.IsNullOrWhiteSpace(webJobsStorage))
             {
                 configOK = false;
                 Console.WriteLine("Please add the Azure Storage account credentials in App.config");
+                Console.Read();
+                return;
             }
-
-            return configOK;
+            else
+            {
+                var host = new JobHost();
+                host.RunAndBlock();
+            }
         }
     }
 }
