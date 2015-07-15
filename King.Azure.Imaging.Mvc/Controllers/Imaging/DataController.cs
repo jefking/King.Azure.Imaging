f50cb0f482ee288912b6066e2c39d3e43d36af8c
Jef King <jef@agilebusinesscloud.com>
2015-07-15 13:21:44 -0700

    Less is more

--- King.Azure.Imaging.Mvc/Controllers/Imaging/DataController.cs
+++ King.Azure.Imaging.Mvc/Controllers/Imaging/DataController.cs
@@ -1,7 +1,11 @@
 ﻿namespace King.Azure.Imaging.Mvc.Controllers.api
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
     using Microsoft.Azure;
 
+    //[Authorize(Roles = "Users")] Uncomment to secure controller
     public class DataController : ImageDataApiController
     {
         private static readonly string connection = CloudConfigurationManager.GetSetting("StorageAccount");
@@ -10,5 +14,15 @@
             : base(connection)
         {
         }
+
+        /*
+        /// Uncomment to secure method
+        [HttpGet]
+        [Authorize(Roles = "Users")]
+        public override async Task<HttpResponseMessage> Get(Guid? id = default(Guid?), string version = null, string file = null)
+        {
+            return await base.Get(id, version, file);
+        }
+        */
     }
 }
--- King.Azure.Imaging.Mvc/Controllers/Imaging/ImageController.cs
+++ King.Azure.Imaging.Mvc/Controllers/Imaging/ImageController.cs
@@ -1,7 +1,8 @@
 ﻿namespace King.Azure.Imaging.Mvc.Controllers.api
 {
     using Microsoft.Azure;
-    
+
+    //[Authorize(Roles = "Users")] Uncomment to secure controller
     public class ImageController : ImageApiController
     {
         private static readonly string connection = CloudConfigurationManager.GetSetting("StorageAccount");
@@ -10,5 +11,23 @@
             : base(connection)
         {
         }
+
+        /*
+        /// Uncomment to secure method
+        [HttpPost]
+        [Authorize(Roles = "Users")]
+        public override async Task<HttpResponseMessage> Post()
+        {
+            return await base.Post();
+        }
+        
+        /// Uncomment to secure method
+        [HttpGet]
+        [Authorize(Roles = "Users")]
+        public override async Task<HttpResponseMessage> Get(string file, ushort width = 0, ushort height = 0, string format = Naming.DefaultExtension, byte quality = Imaging.DefaultImageQuality, bool cache = true)
+        {
+            return await base.Get(file, width, height, format, quality, cache);
+        }
+        */
     }
 }
