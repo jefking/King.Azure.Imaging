0424e32ec754f356bb296d089fe34d84dc923cbf
Jef King <jef@agilebusinesscloud.com>
2015-07-14 23:45:58 -0700

    no comment

--- King.Azure.Imaging.Mvc/Controllers/Imaging/ImageController.cs
+++ King.Azure.Imaging.Mvc/Controllers/Imaging/ImageController.cs
@@ -1,10 +1,7 @@
 ﻿namespace King.Azure.Imaging.Mvc.Controllers.api
 {
     using Microsoft.Azure;
-
-    /// <summary>
-    /// Image Controller
-    /// </summary>
+    
     public class ImageController : ImageApiController
     {
         private static readonly string connection = CloudConfigurationManager.GetSetting("StorageAccount");
