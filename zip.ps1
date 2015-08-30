bd8776667cf8cd3e516a76320c77856c130c8fd9
Jef King <jef@agilebusinesscloud.com>
2015-08-30 15:07:53 -0700

    clean output

--- zip.ps1
+++ zip.ps1
@@ -1,27 +1,27 @@
 $directoryRoot = $args[0]
 $directoryPath = $directoryRoot + "Build\job"
-
+<#
 Write-Host $directoryRoot
 Write-Host $directoryPath
-
+#>
 New-Item -ItemType Directory -Force -Path $directoryPath
 
 $dllPath = $directoryRoot + "King.Azure.Imaging.WebJob\bin\Release\*.dll"
 $exePath = $directoryRoot + "King.Azure.Imaging.WebJob\bin\Release\*.exe"
 $configPath = $directoryRoot + "King.Azure.Imaging.WebJob\bin\Release\*.config"
-
+<#
 Write-Host $dllPath
 Write-Host $exePath
 Write-Host $configPath
-
+#>
 Copy-Item $dllPath $directoryPath
 Copy-Item $exePath $directoryPath
 Copy-Item $configPath $directoryPath
 
 $zipPath = $directoryRoot + "Build\King.Azure.Imaging.zip"
-
+<#
 Write-Host $zipPath
-
+#>
 If (Test-Path $zipPath){
 	Remove-Item $zipPath
 }
