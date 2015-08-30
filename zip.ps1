206baed352973746d7c541b25107ae501ae71220
Jef King <jef@agilebusinesscloud.com>
2015-08-30 13:01:09 -0700

    I hate powershell

--- zip.ps1
+++ zip.ps1
@@ -1,11 +1,30 @@
-New-Item -ItemType Directory -Force -Path .\Build\job
- 
-Copy-Item King.Azure.Imaging.WebJob\bin\Release\*.dll .\Build\job
-Copy-Item King.Azure.Imaging.WebJob\bin\Release\*.exe .\Build\job
-Copy-Item King.Azure.Imaging.WebJob\bin\Release\*.config .\Build\job
+$directoryRoot = $args[0]
+$directoryPath = $directoryRoot + "Build\job"
 
-$directorypath = (Get-Item -Path ".\" -Verbose).FullName + '\Build\job'
-$zipPath =  (Get-Item -Path ".\" -Verbose).FullName + '\Build\King.Azure.Imaging.zip'
+Write-Host $directoryRoot
+Write-Host $directoryPath
+
+New-Item -ItemType Directory -Force -Path $directoryPath
+
+$dllPath = $directoryRoot + "King.Azure.Imaging.WebJob\bin\Release\*.dll"
+$exePath = $directoryRoot + "King.Azure.Imaging.WebJob\bin\Release\*.exe"
+$configPath = $directoryRoot + "King.Azure.Imaging.WebJob\bin\Release\*.config"
+
+Write-Host $dllPath
+Write-Host $exePath
+Write-Host $configPath
+
+Copy-Item $dllPath $directoryPath
+Copy-Item $exePath $directoryPath
+Copy-Item $configPath $directoryPath
+
+$zipPath = $directoryRoot + "Build\King.Azure.Imaging.zip"
+
+Write-Host $zipPath
+
+If (Test-Path $zipPath){
+	Remove-Item $zipPath
+}
 
 Add-Type -A System.IO.Compression.FileSystem
 [IO.Compression.ZipFile]::CreateFromDirectory($directorypath, $zipPath)
